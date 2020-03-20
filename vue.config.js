15711652196d783dc3ec4a76aa92a59665b4bd99
何健松(ex_hejs1) <jiansong1.he@partner.midea.com>
2020-03-20 14:11:43 +0800

    完善多页配置

--- vue.config.js
+++ vue.config.js
@@ -1,38 +1,51 @@
+let moduleName = process.argv[3]
 let path = require('path')
 let glob = require('glob')
+const chalk = require('chalk');
+const AutoDllPlugin = require('autodll-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
 //配置pages多页面获取当前文件夹下的html和js
 function getEntry (globPath) {
-  let entries = {}, basename, tmp;
+  let entries = {}, tmp, modules, moduleName, moduleIndexHtml;
   glob.sync(globPath).forEach(entry => {
     tmp = entry.split('/').splice(-3)
-    basename = tmp[1];
-    entries[basename] = {
-      entry: `src/${tmp[0]}/${tmp[1]}/main.js`,
-      template: `src/${tmp[0]}/${tmp[1]}/${tmp[2]}`,
-      title:  tmp[2],
-			filename: tmp[2]
+    modules = tmp[0];
+    moduleName = tmp[1];
+    moduleIndexHtml = tmp[2];
+    entries[moduleName] = {
+      entry: `src/${modules}/${moduleName}/main.js`,
+      template: `src/${modules}/${moduleName}/${moduleIndexHtml}`,
+      title:  moduleIndexHtml,
+      filename: process.env.NODE_ENV !== 'production'
+      ? `${moduleName}.html`
+      : `${moduleName}/${moduleIndexHtml}`
     };
   });
   return entries;
 }
-
-let pages = getEntry('./src/modules/**/*.html');
-console.log(pages, process.env.NODE_ENV === 'production', path.join(__dirname, './dist'))
+let pages = getEntry(`./src/modules/${moduleName || '**'}/*.html`);
+let moduleKeys = Object.keys(pages).map(key => key)
+// console.log(chalk.yellow(`${process.argv}`))
+if (process.argv[3] && !moduleKeys.includes(process.argv[3])) {
+  console.log(chalk.yellow(`[${process.argv[3]}]模块没有找到，无法进行构建`))
+  return
+}
 //配置end
 
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production', // 禁用eslint,
-  publicPath: '/',
-	productionSourceMap: false,
+  publicPath: process.env.NODE_ENV !== 'production' ? '/' : './',
+  outputDir: 'dist',
+	productionSourceMap: process.env.NODE_ENV !== 'production',
 	pages,
   devServer: {
-    index: 'index.html', //默认启动serve 打开page1页面
-		open: process.platform === 'darwin',
-		host: '',
+    index: 'module1', //默认启动serve 打开page1页面
+		open: false,
+		host: 'localhost',
 		port: 8088,
 		https: false,
     hotOnly: false,
@@ -60,25 +73,33 @@ module.exports = {
 
     config.resolve.alias
       .set('@', resolve('src')) // 配置模块路径公共变量
-      .set('@test', resolve('src/modules/test'))
-
+      .set('@module1', resolve('src/modules/module1'))
+      .set('@module2', resolve('src/modules/module2'))
+    
     Object.keys(pages).forEach(entryName => {
 			config.plugins.delete(`prefetch-${entryName}`);
     });
-    
+
 		if(process.env.NODE_ENV === 'production') {
 			config.plugin('extract-css').tap(() => [{
-				path: path.join(__dirname, './dist'),
-				filename: 'css/[name].[contenthash:8].css'
+				path: path.join(__dirname, `./dist/`),
+				filename: '/css/[name].[contenthash:8].css'
 			}]);
 		}
   },
   configureWebpack: config => {
-		// if(process.env.NODE_ENV === 'production') {
-		// 	config.output = {
-    //     path: path.join(__dirname, './dist'),
-		// 		filename: 'js/[name].[contenthash:8].js'	
-		// 	};
-		// }
-	}
+    let pluginsDev = [
+      new AutoDllPlugin({
+        inject: true, // will inject the DLL bundle to index.html
+        debug: true,
+        filename: 'dll.[name].[contenthash:8].js',
+        path: './dll',
+        entry: {
+          vendor: ['vue', 'vue-router', 'vuex', 'vue-i18n']
+        }
+      }),
+      new HardSourceWebpackPlugin(),
+    ]
+    config.plugins = [...config.plugins, ...pluginsDev]
+  },
 }
