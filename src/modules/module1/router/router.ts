2b86820f8db0a248756e2aff772a9d7e993ca75e
Js.He <465772828@qq.com>
2020-12-11 14:06:33 +0800

    删除无用代码

--- src/modules/module1/router/router.ts
+++ src/modules/module1/router/router.ts
@@ -20,9 +20,6 @@ export default new Router({
     {
       path: '/module1/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "module1-about" */ '@module1/views/About.vue'),
     },
   ],
